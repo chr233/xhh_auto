2145d4b2c22a0d8572425e56cae579e229e6476c
Chr_ <chr@chrxw.com>
2019-07-21 09:41:21 +0800

    修正乱码问题

--- HeyBox_AutoCheck/start.py
+++ HeyBox_AutoCheck/start.py
@@ -3,17 +3,15 @@ from HeyBoxClient import Heybox
 import json
 
 if __name__ =='__main__':
-    dict = json.loads('./config.json')
+    with open('config.json', 'r', encoding='utf-8') as f:
+        dict = json.loads(f.read())
 
     pkey=dict['pkey']
     imei=dict['imei']
     heybox_id=dict['heybox_id']
-
     
     heybox1=Heybox(heybox_id,imei,pkey,1)
 
     heybox1.auto()#自动完成每日任务，自动动态点赞
     heybox1.get_task_stats()
     heybox1.get_task_detail()#获取任务完成情况
-
-    
