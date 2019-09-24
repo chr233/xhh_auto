1563e9bd59c02531c20bf11728cb6e1f9074f6c7
Chr_ <chr@chrxw.com>
2019-09-24 23:38:34 +0800

    pre releases

--- HeyBox_AutoCheck/start.py
+++ HeyBox_AutoCheck/start.py
@@ -18,76 +18,31 @@ import logging
 电邮:chr@chrxw.com
 '''
 
-def main():
-    try:
-        logger=get_logger('start')
-        accountlist=load_accounts_from_file('./accounts.json')
+def start():
+    logger = get_logger('start')
+    accountlist = load_accounts('./accounts.json')
 
-        if accountlist:
-            pass
-
-        i = 0
-        datalist = []
-
-        idlist = []
-        for item in accountlist:
+    if accountlist:
+        for account in accountlist:
+            hbc = HeyboxClient(*account)
             try:
-                idlist.append(item['heybox_id'])
-            except KeyError:
-                continue
-
-        for item in accountlist:
-            i+=1
-            try:
-                pkey = item['pkey']
-                imei = item['imei']
-                heybox_id = int(item['heybox_id'])
-            except KeyError as e:
-                print('第%d个账户实例配置出错，跳过该账户' % i,e)
-                continue
-            
-            hbc = HeyboxClient(heybox_id,imei,pkey,i)
-            try:
-    
                 if is_debug_mode():               
                     #调试模式
                     hbc.sample_do_daily_tasks()
                     if i == len(accountlist):
                         pass
-                    continue
-                
-                #正常逻辑
-                hbc.sample_do_daily_tasks() #完成每日任务
-                hbc.tools_follow_recommand(10,100) #关注推荐关注
-                hbc.tools_follow_followers() #关注粉丝
-                hbc.tools_unfollow_singlefollowers(100) #取关单向关注
-                
-                b = hbc.get_task_stats()#获取任务完成度
-                hbc.get_task_detail()#获取任务详情
-                mydata = hbc.get_my_data()
-                myprofile = hbc.get_my_profile()
-                
-                if mydata and myprofile:
-                    data = list(mydata + myprofile)
-                    data.append('任务全部完成' if b else '任务还未完成')
-                    datalist.append(data)
+                else:
+                    #正常逻辑
+                    hbc.sample_do_daily_tasks() #完成每日任务
+                    hbc.tools_follow_recommand(10,100) #关注推荐关注
+                    hbc.tools_follow_followers() #关注粉丝
+                    hbc.tools_unfollow_singlefollowers(100) #取关单向关注
             except AccountException as e:
                 pass
             except HeyboxException as e:
                 pass
-        try:
-            if datalist :
-                send_to_ftqq(datalist)
-        except KeyError as e:
-            print('未配置SKEY')
+    else:
+        logger.error('有效账号列表为空,请检查是否正确配置了[accounts.json].')
 
-    except ValueError as e:
-        print('出错了',e)
-    except FileNotFoundError as e:
-        print('未找到配置文件，请将config_sample.json重命名为config.json并填入账号凭据',e)
-    except json.JSONDecodeError as e:
-        print('config.json格式有误', e)
-
-
-if __name__ == '__main__':
-    main()
+if __name__=='__main__':
+    start()
